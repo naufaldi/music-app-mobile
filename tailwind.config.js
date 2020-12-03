b4cd39e39dd311b59b9573f0ac83d81a2d9fb747
Naufaldi Rafif <naufaldirafif@Faiks-MacBook-Air.local>
2020-12-04 00:05:18 +0700

    list music

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,30 @@ module.exports = {
   theme: {
     colors: {
       darkblue: "#1E1B2E",
-      'darkblue-secondary': "#27293E",
+      "darkblue-secondary": "#27293E",
       white: "#fff",
       pink: {
         500: "#D44FF0",
       },
       purple: {
         500: "#882DEC",
+        600: "#7623cf",
+      },
+      blue: {
+        500: "#5185F5",
+        600: "#4625E6",
+      },
+      red: {
+        500: "#FF7E9D",
+        600: "#CB0D3A",
+      },
+      orange: {
+        500: "#FFA133",
+        600: "#EC442D",
+      },
+      green: {
+        500: "#22C55E",
+        600: "#16A34A",
       },
     },
     extend: {},
